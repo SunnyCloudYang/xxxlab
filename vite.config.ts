a279c72c43a8735b3821487de49431c1feef5c81
SunnyCloudYang <sunnycloudyang@outlook.com>
2025-08-30 16:28:11 +0800

    fix: fix static assets path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/xxxlab/", // Add prefix to all static asset paths
   build: {
     outDir: "docs",
     emptyOutDir: true, // Clean the output directory before each build
